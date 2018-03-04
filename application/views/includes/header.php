400239801acf5bb2cf527efc26ec71130f7cd6a5
tsyogesh40 <tsyogesh40@gmail.com>
2018-03-04 18:19:27 +0530

    Scroll Fixed

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -96,7 +96,7 @@
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
           <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu" style="max-height: 620px;;overflow-y: auto;">
+          <ul class="sidebar-menu" style="max-height: 620px;overflow-y: auto;">
             <li class="header">MAIN NAVIGATION</li>
             <li class="treeview">
               <a href="<?php echo base_url(); ?>dashboard">
