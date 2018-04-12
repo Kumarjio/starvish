8e9a03ba1217c9d3274804175955fef7bc1b54d3
tsyogesh40 <tsyogesh40@gmail.com>
2018-04-12 10:34:14 +0530

    quotation update

--- application/views/customer/customer_quotation_view.php
+++ application/views/customer/customer_quotation_view.php
@@ -24,7 +24,33 @@
                     <div class="row">
                     <div class="col-lg-10 col-lg-offset-1 col-xs-12 col-sm-12 col-md-12">
                     <caption></caption>
-                    
+                    <?php print_r($datas);?>
+                    <div class="table-responsive">
+                    <table class="table table-striped table-condensed table-bordered">
+                        <thead class="bg-primary">
+                          <tr>
+                          <th>Customer ID</th>
+                          <th><php ?></th>
+                          </tr>
+                        </thead>
+                        <tbody class="bg-info">
+                          <tr>
+                            <th>Quote ID</th>
+                            <td id="quote_id"></td>
+                          </tr>
+                          <tr>
+                          <th>Date</th>
+                          <td id="date"></td>
+                        </tr>
+                        <tr>
+                        <th>Description</th>
+                        <td id="description"></td>
+                        </tr>
+                        </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
 
 
                     </div><!-- /.box-body -->
